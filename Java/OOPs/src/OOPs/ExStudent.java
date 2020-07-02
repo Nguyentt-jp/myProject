b74fdbfe08496f28223d09ac7c5a42019a921830
Nguyentt-jp <Nguyentt-jp@users.noreply.github.com>
2020-07-02 20:25:09 +0700

    fg
    fg

--- Java/OOPs/src/OOPs/ExStudent.java
+++ Java/OOPs/src/OOPs/ExStudent.java
@@ -1,5 +1,12 @@
 package OOPs;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+
 public class ExStudent {
 	int id;
 	String name;
@@ -13,6 +20,8 @@ public class ExStudent {
 		System.out.println(id + " " + name);
 	}
 
+
+
 	public static void main(String[] args) {
 		ExStudent obj = new ExStudent();
 		System.out.println(obj.id);
@@ -24,8 +33,16 @@ public class ExStudent {
 
 		obj.displayInfo();
 		obj1.displayInfo();
-		
+		ArrayList<Integer> integerArrayList = new ArrayList<>();
+		integerArrayList.add(1);
+		integerArrayList.add(2);
+
+		for (Integer integer : integerArrayList) {
+			System.out.println(integer);
+		}
+		Random ran = new Random();
+		ArrayList<Integer> arraylist = new ArrayList<>();
+
 
 	}
-
 }
